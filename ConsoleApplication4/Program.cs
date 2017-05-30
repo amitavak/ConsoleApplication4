30ff5c73d4f813d0fbcc5cb3a905fffc89c379e0
Amitava Karan <amitava.karan@ggktech.com>
2017-05-30 15:40:21 +0530

    fb-3: Add company in department class.

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -57,5 +57,7 @@ namespace ConsoleApplication4
         public int DepartmentId { get; set; }
 
         public string DepartmentName { get; set; }
+
+        public Company Company { get; set; }
     }
 }
