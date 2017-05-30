128f7dfc98828af2ebe3e62da380b38b924bf667
Amitava Karan <amitava.karan@ggktech.com>
2017-05-30 15:35:59 +0530

    Added website in company class.

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -44,5 +44,7 @@ namespace ConsoleApplication4
         public int CompanyId { get; set; }
 
         public string CompanyName { get; set; }
+
+        public string Website { get; set; }
     }
 }
